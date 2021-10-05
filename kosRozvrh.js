8d62e50b7e303dbc45febf923b293d5951d2e6d6
webik150 <agenttucnak@gmail.com>
2021-10-05 23:49:11 +0200

    fix some naming

--- kosRozvrh.js
+++ kosRozvrh.js
@@ -25,12 +25,6 @@ function dayFromPosition(listek) {
     }
 }
 
-//Append ICal script to head
-$("head").append('<script async defer type="text/javascript" src="https://unpkg.com/ical.js@1.4.0/build/ical.js"></script>');
-//Parse the table after the script loads
-whenAvailable("ICAL", function(t) {
-    generateICal();
-});
 //Generate the actual ICal file
 function generateICal() {
     var comp = new ICAL.Component(['vcalendar', [],
@@ -39,16 +33,16 @@ function generateICal() {
     comp.updatePropertyWithValue('prodid', '-//AMU KOS Schedule generator');
 
     //Divs containing info about the classes
-    let listky = $("#tabRozvrh [id^=detaillistek");
-    let pozicniListky = $("#tabRozvrh [id^=listek");
+    let classes = $("#tabRozvrh [id^=detaillistek");
+    let locations = $("#tabRozvrh [id^=listek");
 
     //Go through all of them and separate free text from text in tags
-    for (let i = 0; i < listky.length; i++) {
-        let separators = $(listky.get(i)).children().contents().filter(function() {
+    for (let i = 0; i < classes.length; i++) {
+        let separators = $(classes.get(i)).children().contents().filter(function() {
             return this.nodeType == 1;
         });
         let info = [];
-        let nadpisy = $(listky.get(i)).children().contents().filter(function() {
+        let nadpisy = $(classes.get(i)).children().contents().filter(function() {
             return this.nodeType == 3;
         }).map(function(i, val) {
             return val.nodeValue.replace(/[:\-,\(\)]|[0-9]/g, "").trim(); //Remove special chars
@@ -79,17 +73,18 @@ function generateICal() {
         let vevent = new ICAL.Component('vevent'),
             ev = new ICAL.Event(vevent);
 
+		// Split time string into start and end time, and into hours and minutes.
         let times = texty[7].value.split("-");
         let startTime = times[0].split(":");
         let endTime = times[1].split(":");
 
-        // Set standard properties
+        // Set event properties
         ev.summary = texty[1].value;
         ev.description = texty[4].value + ", " + texty[3].value
         ev.uid = texty[0].value;
 
         ev.startDate = ICAL.Time.now().startOfWeek(2);
-        ev.startDate.day += pozicniListky[i].offsetTop / 120;
+        ev.startDate.day += locations[i].offsetTop / 120;
         ev.startDate.hour = parseInt(startTime[0]);
         ev.startDate.minute = parseInt(startTime[1]);
         ev.startDate.wrappedJSObject.isDate = false;
@@ -97,7 +92,7 @@ function generateICal() {
         ev.startDate = ICAL.Time.fromString(ev.startDate.toString());
 
         ev.endDate = ICAL.Time.now().startOfWeek(2);
-        ev.endDate.day += pozicniListky[i].offsetTop / 120;
+        ev.endDate.day += locations[i].offsetTop / 120;
         ev.endDate.hour = parseInt(endTime[0]);
         ev.endDate.minute = parseInt(endTime[1]);
         ev.endDate.wrappedJSObject.isDate = false;
@@ -105,7 +100,7 @@ function generateICal() {
         ev.endDate = ICAL.Time.fromString(ev.endDate.toString());
 
 		//Configure weekly
-        vevent.addPropertyWithValue('RRULE', 'FREQ=WEEKLY;BYDAY=' + dayFromPosition(pozicniListky[i]) + ';INTERVAL=1');
+        vevent.addPropertyWithValue('RRULE', 'FREQ=WEEKLY;BYDAY=' + dayFromPosition(locations[i]) + ';INTERVAL=1');
 
         // Add the new component
         comp.addSubcomponent(vevent);
@@ -115,3 +110,11 @@ function generateICal() {
     //Cleanup
     $('script[src="https://unpkg.com/ical.js@1.4.0/build/ical.js"]').remove();
 }
+
+//Append ICal script to head
+$("head").append('<script async defer type="text/javascript" src="https://unpkg.com/ical.js@1.4.0/build/ical.js"></script>');
+
+//Parse the table after the script loads
+whenAvailable("ICAL", function(t) {
+    generateICal();
+});
